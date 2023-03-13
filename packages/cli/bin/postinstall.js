c92e70d7d834b0876947fde2516c3147ae5dc977
Vladimir Kalmykov <vladimirkalmykovdev@gmail.com>
2023-03-13 11:00:52 +0300

    fix: create system path stats after install

--- packages/cli/bin/postinstall.js
+++ packages/cli/bin/postinstall.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
 const fs = require("fs");
-const resolveSystenPath = require("@invoke-script/core/resolve-system-path");
-const systemPath = resolveSystenPath();
-const systemPathScripts = resolveSystenPath("scripts");
+const resolveSystemPath = require("@invoke-script/core/resolve-system-path");
+const {
+  SYSTEM_STATS_FILENAME
+} = require("@invoke-script/core/constants");
+const systemPath = resolveSystemPath();
+const systemPathScripts = resolveSystemPath("scripts");
+const systemPathStats = resolveSystemPath(SYSTEM_STATS_FILENAME);
+
 
 if (!fs.existsSync(systemPath)) {
   fs.mkdirSync(systemPath);
+  console.log("System path: CREATED");
+} else {
+  console.log("System path: OK");
 }
 
 if (!fs.existsSync(systemPathScripts)) {
   fs.mkdirSync(systemPathScripts);
+  console.log("System path scripts: CREATED");
+} else {
+  console.log("System path scripts: OK");
+}
+
+if (!fs.existsSync(systemPathStats)) {
+  fs.writeFileSync(systemPathStats, "{}");
+  console.log("System path stats: CREATED");
+} else {
+  console.log("System path stats: OK");
 }
